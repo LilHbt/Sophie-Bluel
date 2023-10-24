4954a3ee50b1a6b523359a3fae725e522b89b215
LilHbt <lilianhumblot@gmail.com>
2023-10-24 11:24:42 +0200

    affichage projet et filtres terminé

---
+++ Sophie/FrontEnd/JS/acceuil.js
@@ -0,0 +1,19 @@
+import { creerFiltres, afficherWorks } from "./functions.js"
+
+//Recuperation depuis l'api//
+var works = fetch('http://localhost:5678/api/works')
+            .then(works => works.json())
+
+var categories = fetch('http://localhost:5678/api/categories')
+            .then(categories => categories.json())            
+
+//Creation des projets dans le Dom//
+ //creations des boutton filtres/
+categories.then (categories=>{
+         creerFiltres(categories)
+})   
+//generation des projets en utilisant les données de l'api//
+works.then(works => {
+    afficherWorks(works) 
+})
+
