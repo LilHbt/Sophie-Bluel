db57d96e960ef088155bd08f6dc791af983716f6
LilHbt <lilianhumblot@gmail.com>
2023-10-27 10:24:47 +0200

    Update login.js

--- Sophie/FrontEnd/JS/login.js
+++ Sophie/FrontEnd/JS/login.js
@@ -29,7 +29,7 @@ form.addEventListener("submit", (event) => {
     }
   });
 });
-//ajouter le toke au local storage//
+//ajouter le token au local storage//
 async function connexion(login) {
   try {
     const response = await fetch("http://localhost:5678/api/users/login", {
